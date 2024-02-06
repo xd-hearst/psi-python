8375f3674a66774dcc11b201a63f34e7b3b274cd
xd-hearst <xun.ding@hearst.com>
2024-02-06 16:23:02 -0600

    append data

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ _SYNOPSIS_
 
 1. This script reads urls from 'pagespeed.txt' file. Load this file with full URLS.
 2. Real time user experience metrics are recorded
+
+"CLS", "LCP", "FID", "FCP", "TTFB", "fid"
--- pagespeed-api.py
+++ pagespeed-api.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from dataparse import parseData
 
 load_dotenv()
 apiKey = os.getenv("API_KEY")
@@ -9,16 +10,29 @@ apiKey = os.getenv("API_KEY")
 
 # JSON paths: https://developers.google.com/speed/docs/insights/v4/reference/pagespeedapi/runpagespeed
 
+def printMetrics(metricNames):
+    l = ''
+    for metric in metricNames:
+        names = [f'{metric}-percentile', f'{metric}-good', f'{metric}-needsImprov', f'{metric}-bad']
+        l += ','.join(names)
+    return l
+
+filename = 'pagespeed-results-mobile.csv'
+metricNames= ["CLS", "LCP", "FID", "FCP", "TTFB", "fid"]
+columnTitleRow = f'URL,analysisUTCTimestamp,{printMetrics(metricNames)}\n'
+
+if not os.path.exists(filename):
+    with open(filename, 'w', newline='') as f:
+        f.write(columnTitleRow)
+
+
 # Populate 'pagespeed.txt' file with URLs to query against API.
 with open('pagespeed.txt') as pagespeedurls:
-    download_dir = 'pagespeed-results.csv'
-    file = open(download_dir, 'w')
+    download_dir = 'pagespeed-results-mobile.csv'
+    file = open(download_dir, 'a')
     content = pagespeedurls.readlines()
     content = [line.rstrip('\n') for line in content]
-
-    columnTitleRow = "URL, realtimecls, <0.1cls, .1-.25cls,.25>cls, First Contentful Paint, First Interactive\n"
-    file.write(columnTitleRow)
-
+  
     # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
     for line in content:
         # If no "strategy" parameter is included, the query by default returns desktop data.
@@ -28,45 +42,8 @@ with open('pagespeed.txt') as pagespeedurls:
         final = r.json()
         
         try:
-            urlid = final['id']
-            split = urlid.split('?') # This splits the absolute url from the api key parameter
-            urlid = split[0] # This reassigns urlid to the absolute url
-            ID = f'URL ~ {urlid}'
-            ID2 = str(urlid)
-            urlfcp = final['lighthouseResult']['audits']['first-contentful-paint']['displayValue']
-            FCP = f'First Contentful Paint ~ {str(urlfcp)}'
-            clsx = final["loadingExperience"]["metrics"]["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["percentile"]/100
-            clsGood= final["loadingExperience"]["metrics"]["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["distributions"]["proportion"][0]
-            clsNeedsImprov= final["loadingExperience"]["metrics"]["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["distributions"]["proportion"][1]
-            clsBad= final["loadingExperience"]["metrics"]["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["distributions"]["proportion"][2]
-            fcpx = final["loadingExperience"]["metrics"]["FIRST_CONTENTFUL_PAINT_MS"]["percentile"] #into seconds (/1000)
-            fidx = final["loadingExperience"]["metrics"]["FIRST_INPUT_DELAY_MS"]["percentile"] #into seconds (/1000)
-            lcpx = final["loadingExperience"]["metrics"]["LARGEST_CONTENTFUL_PAINT_MS"]["percentile"]
-            clsx = final["loadingExperience"]["metrics"]["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["percentile"]/100
-
-            FCP2 = str(urlfcp)
-            urlfi = final['lighthouseResult']['audits']['interactive']['displayValue']
-            FI = f'First Interactive ~ {str(urlfi)}'
-            FI2 = str(urlfi)
-        except KeyError:
-            print(f'<KeyError> One or more keys not found {line}.')
-        
-        try:
-            row = f'{clsx},{clsGood},{clsNeedsImprov},{clsBad},{ID2},{FCP2},{FI2}\n'
+            row = parseData(final, line)
             file.write(row)
         except NameError:
             print(f'<NameError> Failing because of KeyError {line}.')
             file.write(f'<KeyError> & <NameError> Failing because of nonexistant Key ~ {line}.' + '\n')
-        
-        try:
-            print(ID) 
-            print(FCP)
-            print(FI)
-            print(f'FCP ~ {fcpx} seconds')
-            print(f'Fidx ~ {fidx} seconds')
-            print(f'lcpx ~ {lcpx} seconds')
-            print(f'clsx ~ {clsx} seconds')
-        except NameError:
-            print(f'<NameError> Failing because of KeyError {line}.')
-
-    file.close()
